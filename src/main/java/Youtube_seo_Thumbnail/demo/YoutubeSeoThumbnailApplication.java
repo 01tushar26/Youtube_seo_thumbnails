1855fd6fb22dfbba5b9f2b096989d9405a11e7fc
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-01-02 10:47:48 +0530

    first commit

---
+++ src/main/java/Youtube_seo_Thumbnail/demo/YoutubeSeoThumbnailApplication.java
@@ -0,0 +1,13 @@
+package Youtube_seo_Thumbnail.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeSeoThumbnailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeSeoThumbnailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Youtube_seo_Thumbnail/demo/YoutubeSeoThumbnailApplicationTests.java
@@ -0,0 +1,13 @@
+package Youtube_seo_Thumbnail.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeSeoThumbnailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
